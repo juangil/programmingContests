c4d23cc1aa28d144a6f9b9affc08a33b61a947f2
Juan David <juangilopez@gmail.com>
2013-07-16 16:45:33 -0500

    Update cutSilverBar.cpp

--- clasificatorias2013/cutSilverBar.cpp
+++ clasificatorias2013/cutSilverBar.cpp
@@ -6,5 +6,16 @@
 
 using namespace std;
 
+int myf(int n){
+          int cont = 0;
+          while(n--){cont++; n/=2}
+          return cont;
+}
+
 int main(){
-     
+     int n;
+     while(cin >> n and n){
+          cout<<myf(n) - 1<<endl
+     }
+     return 0;
+}
