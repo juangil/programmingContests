f961e63b21fc2ae2302135cfc5e4b3daa03efa8d
Juan David <juangilopez@gmail.com>
2014-02-13 10:59:13 -0500

    freeParentheses accepted

--- dynamicProgramming/freeParentheses.cpp
+++ dynamicProgramming/freeParentheses.cpp
@@ -2,10 +2,10 @@
 # define D(x) cout<<(x)<<endl;
 using namespace std;
 
-const int MAXVALUES = 7001;
+const int MAXVALUES = 7002;
 const int MAXNUMS = 32;
 
-int used [MAXVALUES];
+bool used [MAXVALUES];
 bool dp[MAXNUMS][MAXNUMS][MAXVALUES];
 int sig[MAXNUMS];
 int num[MAXNUMS];
@@ -14,16 +14,16 @@ int N;
 template <class T> int toInt(const T &x)
 { stringstream s; s << x; int r; s >> r; return r; }
 
+
 void solve(int idx, int open, int val){
-    //cout<<(dp[idx][open][val + 3500] == 0)<<endl;
     if(dp[idx][open][val + 3500]) return;
     dp[idx][open][val + 3500] = 1;
     if(idx == N){
-        //cout<<"pass"<<endl;
         used[val + 3500] = 1;
         return;
     }
-    int new_val = val + num[idx]*sig[idx]*((open%2)? -1:1);
+    int new_val = val + num[idx] * sig[idx] * ((open%2)? -1:1);
+    
     if(sig[idx] == -1)
         solve(idx+1, open+1, new_val);
     if(open > 0)
@@ -35,15 +35,17 @@ void solve(int idx, int open, int val){
 int main(){
     string expr;
     while(getline(cin, expr)){
+        if(expr.size() <= 1) break;
         stringstream ss(expr);
         string mio;
         int i = 0;
-        int j = 0;
+        int j = 1;
         int tmp;
+        sig[0] = 1;
         while(ss>>mio) {
             if(mio != "+" && mio != "-"){
                 tmp = toInt(mio);
-                num[i] = tmp*((i == 0)? 1:sig[i-1]);
+                num[i] = tmp;
                 i++;
             }
             else{
@@ -52,31 +54,25 @@ int main(){
                 j++;
             }
         }
-        int totalplus = num[0];
-        for(int k = 0; k < i; ++k)
-            totalplus += num[k];
         
-        totalplus += 3500;
-        
-        for(int k = 0; k < totalplus; ++k)
+        for(int k = 0; k <= MAXVALUES; ++k)
             used[k] = 0;
         
-        for(int k = 0; k < i; ++k)
-            for(int m = 0; m < i; ++m)
+        for(int k = 0; k < MAXNUMS; ++k)
+            for(int m = 0; m < MAXNUMS; ++m)
                 for(int l = 0; l < MAXVALUES; ++l)    
-                    dp[k][m][l] = false;
+                    dp[k][m][l] = 0;
 
-        
         N = i;
         solve(0,0,0);
         int cont = 0;
-        for(int k = 0; k <= totalplus; ++k)    
+        for(int k = 0; k < MAXVALUES; ++k)
             cont += used[k];
         
         cout<<cont<<endl;
     }
+    return 0;
 
 
 }
-
 
